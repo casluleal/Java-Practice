aac7a9e762c362c25b57c0c10b00da42aa01db63
Lucas Leal <luucas.leal@hotmail.com>
2019-03-04 20:48:40 -0300

    Inventory testcase finalized

--- ProductInventory/src/Tests/InventoryTest.java
+++ ProductInventory/src/Tests/InventoryTest.java
@@ -1,8 +1,17 @@
 package Tests;
 
+import Classes.Inventory;
+import Classes.Product;
 import junit.framework.TestCase;
 
 public class InventoryTest extends TestCase {
 
+    public void testSumPrices() {
+        Inventory i = new Inventory();
+        i.addProduct(new Product(1, "Apple", 2.5, 1));
+        i.addProduct(new Product(2, "Strawberry", 3, 5));
+        i.addProduct(new Product(3, "Banana", 5, 1));
 
+        assertEquals(i.sumPrices(), 22.50);
+    }
 }
